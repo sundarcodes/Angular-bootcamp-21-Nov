65ba6509fee2cf448605825a614a8c3775f0b983
sundarcodes <sundar82@gmail.com>
2017-11-24 12:37:27 +0530

    Showing list of books from home component works

---
+++ my-shelf/src/app/core/models/book.ts
@@ -0,0 +1,10 @@
+export class Book {
+    id: number;
+    title: string;
+    description: string;
+        // url: 'http://shop.oreilly.com/product/9780596007126.do',
+        // category: 'Book',
+        // description: 'Your Brain on Design Patterns',
+        // isRead: boolean,
+        // id: number;
+}
--- my-shelf/src/app/home/home.component.ts
+++ my-shelf/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { Book } from './../core/models/book';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,8 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  books: Book[];
+  constructor() {
+    let uniqueId = 1;
+    this.books = [
+      {
+        title: 'Refactoring',
+        description: 'Techinques to refactor your code',
+        id: uniqueId++
+      },
+      {
+        title: 'Functional Programming',
+        description: 'Practical introduction to functional programming',
+        id: uniqueId++
+      },
+      {
+        title: 'No SQL Distilled',
+        description: 'A brief guide to emerging world of polygot persistence',
+        id: uniqueId++
+      },
+      {
+        title: 'Redux',
+        description: 'A Cartoon introduction to Redux',
+        id: uniqueId++
+      },
+      {
+        title: 'Head First Design Patterns',
+        description: 'Your Brain on Design Patterns',
+        id: uniqueId++
+      },
+      {
+        title: 'Game Programming Patterns',
+        description: 'Design Patterns for Game development',
+        id: uniqueId++
+      }
+    ];
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }
