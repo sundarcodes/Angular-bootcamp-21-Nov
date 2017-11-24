1aa85121ba94677ccd931db951d0a4b0858e450e
sundarcodes <sundar82@gmail.com>
2017-11-24 12:22:55 +0530

    1. Added book card component 2. Layout for home component done

--- my-shelf/src/app/app.module.ts
+++ my-shelf/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { BookCardComponent } from './book-card/book-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    BookCardComponent
   ],
   imports: [
     BrowserModule
---
+++ my-shelf/src/app/book-card/book-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-book-card',
+  templateUrl: './book-card.component.html',
+  styleUrls: ['./book-card.component.css']
+})
+export class BookCardComponent implements OnInit {
+
+  @Input() title: string;
+  @Input() description: string;
+  constructor() {
+   }
+
+  ngOnInit() {
+  }
+
+}
